78f05cc077637ccd79d24cd07be8b014089a20fb
Sandip  Pradhan <158505928+sandip9851@users.noreply.github.com>
2024-06-28 11:30:05 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,8 +2,32 @@
 const output = document.getElementById("output");
 const btn = document.getElementById("download-images-button");
 
+
 const images = [
   { url: "https://picsum.photos/id/237/200/300" },
   { url: "https://picsum.photos/id/238/200/300" },
   { url: "https://picsum.photos/id/239/200/300" },
 ];
+
+btn.addEventListener("click",()=>{
+	displayImg(images)
+})
+function displayImg(images) {
+	for (let val of images) {
+		return new Promise((resolve,reject)=>{
+			let displayImg = new Image;
+			displayImg.src = val.url;
+			displayImg.onload = ()=>{
+				resolve(displayImg)
+			}
+			displayImg.onerror = ()=>{
+				reject(new Error(`Failed to load image's URL: ${val.url}`))
+			}
+		}).then((img)=>{
+			output.append(img)
+		}).catch((error)=>{
+			 output.append(error.message);
+		}
+	}
+	
+}
